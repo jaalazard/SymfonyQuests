562e636326955ec3e775347dbd9b89b20c238f84
jaalazard <ja.alazard@proton.me>
2023-06-10 18:27:47 +0200

    quete25

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,14 +6,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\ProgramRepository;
 
 
 class DefaultController extends AbstractController
 
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    #[Route('/', name: 'app_index')]
+    public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('index.html.twig',);
+        $program = $programRepository->findBy([], ['id' => 'DESC'], 6);
+
+        return $this->render('index.html.twig', [
+            'program' => $program,
+        ]);
+    }
+
+    #[Route('/myprofile', name: 'compte')]
+    public function account(): Response
+    {
+        $user = $this->getUser();
+        return $this->render('account.html.twig', ['user' => $user]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,12 +4,14 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -34,6 +36,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\OneToMany(mappedBy: 'owner', targetEntity: Program::class)]
     private Collection $programs;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -141,6 +146,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
                 $program->setOwner(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
