b560f9eafb27937ab37780d3cabace33081c97d2
jaalazard <ja.alazard@proton.me>
2023-06-20 19:06:14 +0200

    quete27

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,6 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\Swup\SwupBundle::class => ['all' => true],
+    Symfony\UX\Typed\TypedBundle::class => ['all' => true],
 ];
